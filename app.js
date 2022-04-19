3b85c77a8649efd4a8dbe7ce2aad0ee495c6afbe
Leon <johnatan.ranon@yahoo.com>
2022-04-19 21:26:05 +0800

    Add EJS

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-    
-    var today = new Date();
+app.set("view engine", "ejs");
 
-    if (today.getDay() === 6 || today.getDay() === 0 {
-        res.send("Yay it's the weekend");
-    } else {
-        res.send("Boo! I have to work. ")
-    }
+app.get("/", function(req, res){
+
+    var today = new Date();
+    var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var day = weekday[today.getDay()];
+
+    res.render("list", {kindOfDay: day});
 
 });
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
-
