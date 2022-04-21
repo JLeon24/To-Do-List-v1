ba0631dc995ad55398f747cf951672bf724937ae
Leon <johnatan.ranon@yahoo.com>
2022-04-21 18:55:19 +0800

    Complete the code to pass data from webpage to server

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
-// Required code to run express
+// Required code to use express, bodyParser
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 
 // Required code to use EJS
 app.set("view engine", "ejs");
 
-// 
-app.get("/", function(req, res){
+// code to use body parser to get the value from the textbox
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-    var today = new Date(); // Get the date for today
-    var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; // Created an array to get the name of the day
-    var day = weekday[today.getDay()]; //Get the day today
+// Create a variable array to hold the items.
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
-    res.render("list", {
-        kindOfDay: day
-    }); //Pass the day today to the EJS file
+//------------------------------------------------------------------
+
+//Load up to home page, to view the current date and list items
+
+//Express code. Handles the GET request
+app.get("/", function(req, res) {
+
+  // Create a variable to hold the the date and time (method)
+  var today = new Date();
+
+  //Create a variable to hold an Object to format the date
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    day: "numeric",
+    month: "long"
+  }
+
+  //Create a variable to hold the date to string format
+  var day = today.toLocaleDateString("en-US", options);
+
+  // Render EJS and Pass the var day (kindOfDay) and items(newListItems) to the FrontEnd (EJS file)
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
 
 });
 
+//------------------------------------------------------------------
+
+//Express code. Handles the POST request, it will catch the data from newItem in FrontEnd
+app.post("/", function(req, res) {
+  var item = req.body.newItem; // the value of the newItem is stored to var item. Used bodyparser to get the value.
+  items.push(item); // add the item to the array items
+  res.redirect("/"); // redirect to the home route
+});
+
+
 // Confirmation for the node server
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
