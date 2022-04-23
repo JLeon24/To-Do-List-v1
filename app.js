0c6cb6fd9170c83cad5981f8616f7cc63d26f210
Leon <johnatan.ranon@yahoo.com>
2022-04-23 09:25:43 +0800

    Add html css

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 // Required codes to run express, body parser, ejs
+
 const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
-var port = 3000; // Created a variable and assign a value of the port to use
+let port = 3000; // Created a variable and assign a value of the port to use
 
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({ // Used to parse data from the webpage
   extended: true
 }));
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public")); // Location for static files
 
-var items = ["Buy Food", "Cook Food", "Eat Food"]; //Created a variable to store initial list items
+let items = ["Buy Food", "Cook Food", "Eat Food"]; //Created a variable to store initial list items
+let workItems = [];
 
 //--------------- Load up to home page, to view the current date and list items
 
-
 app.get("/", (req, res) => {
+  let today = new Date(); // Created a variable to store the date today (method)
 
-  var today = new Date(); // Created a variable to store the date today (method)
-
-  //Created an object and assigned to a variable. Use to format the date
-  var options = {
+  //Created an object and assigned to a variable. Used to format the date
+  let options = {
     weekday: "long",
     year: "numeric",
     day: "numeric",
     month: "long"
   }
 
-  // Created a variable and assign the var options as a callback to the object today.toLocaleDateString("en-US", options
-  var day = today.toLocaleDateString("en-US", options);
+  // added the object options as an argument and a callback to the variable day
+  let day = today.toLocaleDateString("en-US", options);
 
-  // Render EJS and Pass the var day (kindOfDay) and items(newListItems) to the FrontEnd (EJS file)
+  // Render EJS and pass the value of day (kindOfDay) and items(newListItems) to the FrontEnd (EJS file)
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 
@@ -43,11 +44,11 @@ app.get("/", (req, res) => {
 //--------------- Load up to home page, to view the current date and list items
 
 //Express code. Handles the POST request, it will catch the data from newItem in FrontEnd
-app.post ("/", (req, res) => {
-  var item = req.body.newItem; // the value of the newItem is stored to var item using body parser
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem; // the value of the newItem is stored to item using body parser
   items.push(item); // Push the item value to the items array
   res.redirect("/"); // Redirect to the home route
-
 });
 
 
